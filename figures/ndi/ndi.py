8be3def4a23f03cc243b6c0e86c6408a0eb1ad9c
hwalton <harveywalton.hw@gmail.com>
2023-11-22 17:16:14 +0000

    chatgpt output figures added

--- figures/ndi/ndi.py
+++ figures/ndi/ndi.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
+plt.figure(figsize=(8, 4.8))
+
 # Define the range and density of the x-axis
 x = np.linspace(-4, 4, 1000)
 
@@ -13,8 +15,8 @@ x_fill = np.linspace(-2, 1, 500)  # Adjust -4 and 1 to your range
 y_fill = norm.pdf(x_fill)
 
 # Create the plot
-plt.plot(x, y, label='Normal Distribution')
-plt.fill_between(x_fill, y_fill, alpha=0.5, label='Definite Integral')
+plt.plot(x, y, color='blue', label='Normal Distribution')
+plt.fill_between(x_fill, y_fill, alpha=0.5, color='blue', label='Definite Integral')
 
 # Add labels and title
 plt.xlabel('x')
@@ -23,5 +25,5 @@ plt.title('Definite Integral of the Normal Distribution')
 plt.legend()
 
 # Save the figure
-plt.savefig('figures/ndi/ndi.png')
+plt.savefig('ndi.png')
 plt.show()
