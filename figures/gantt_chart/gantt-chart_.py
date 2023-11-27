af6ee795794809e36162fb06d109033694904af9
hwalton <harveywalton.hw@gmail.com>
2023-11-27 16:19:56 +0000

    +

--- figures/gantt_chart/gantt-chart_.py
+++ figures/gantt_chart/gantt-chart_.py
@@ -1,43 +1,62 @@
 import pandas as pd
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 import datetime as dt
 
-df = pd.DataFrame({'task': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
-                   'start': pd.to_datetime(['20 Oct 2022', '24 Oct 2022', '26 Oct 2022', '31 Oct 2022', '3 Nov 2022', '7 Nov 2022', '10 Nov 2022', '14 Nov 2022', '18 Nov 2022', '23 Nov 2022', '28 Nov 2022', '30 Nov 2022']),
-                   'end': pd.to_datetime(['31 Oct 2022', '28 Oct 2022', '31 Oct 2022', '8 Nov 2022', '9 Nov 2022', '18 Nov 2022', '17 Nov 2022', '22 Nov 2022', '23 Nov 2022', '1 Dec 2022', '5 Dec 2022', '5 Dec 2022']),
-                   'completion_frac': [1, 1, 1, 1, 1, 0.95, 0.7, 0.35, 0.1, 0, 0, 0]})
-print(df)
+plt.rcParams.update({'font.size': 12})
+
+df = pd.DataFrame({
+    'task': ['XMAS HOLIDAY AND EXAM PERIOD',
+             'Adjust the model to enable the closed\nform Fourier Transform to be found.',
+             'Compute FRF for both methods.',
+             'Compute MSE between\nthe two methods.',
+             'Add Gaussian Noise\nand compare the MSE.',
+             'Repeat for new signal\nof pure sine wave.',
+             'EASTER HOLIDAY',
+             'Write up conclusions\nin report.'],
+    'start': pd.to_datetime(['18 DEC 2023', '5 FEB 2024', '19 FEB 2024', '4 MAR 2024', '11 MAR 2024', '18 MAR 2024', '25 MAR 2024', '15 APR 2024'], format='%d %b %Y'),
+    'end': pd.to_datetime(['4 FEB 2024', '18 FEB 2024', '3 MAR 2024', '10 MAR 2024', '17 MAR 2024', '24 MAR 2024', '14 APR 2024', '1 MAY 2024'], format='%d %b %Y')
+})
 
 df['days_to_start'] = (df['start'] - df['start'].min()).dt.days
-
 df['days_to_end'] = (df['end'] - df['start'].min()).dt.days
+df['task_duration'] = df['days_to_end'] - df['days_to_start'] + 1
 
-df['task_duration'] = df['days_to_end'] - df['days_to_start'] + 1  # to include also the end date
+fig, ax = plt.subplots(figsize=(10, 6))
 
-df['completion_days'] = df['completion_frac'] * df['task_duration']
+for i in range(len(df)):
+    color = 'red' if df.iloc[i]['task'] in ['XMAS HOLIDAY AND EXAM PERIOD', 'EASTER HOLIDAY'] else 'blue'
+    ax.barh(y=df.iloc[i]['task'], width=df.iloc[i]['task_duration'], left=df.iloc[i]['days_to_start'], color=color)
 
+ax.invert_yaxis()
 
-# 1
-fig, ax = plt.subplots()
+ax.set_title('Future Work Time Plan', fontsize=15)
 
-plt.barh(y=df['task'], width=df['task_duration'], left=df['days_to_start'] + 1)
-plt.title('Project Management Schedule of Project X', fontsize=15)
+start_date = df['start'].min()
+end_date = df['end'].max()
 
-# 2
-plt.gca().invert_yaxis()
+extended_end_date = max(end_date, df['end'].max()) + pd.Timedelta(days=7)
+mondays = pd.date_range(start=start_date, end=extended_end_date, freq='W-MON')
 
-# 3
-xticks = np.arange(5, df['days_to_end'].max() + 2, 7)
+xticks = (mondays - start_date).days
+xticklabels = mondays.strftime("%d %b %y")
 
-# 4
-xticklabels = pd.date_range(start=df['start'].min() + dt.timedelta(days=4), end=df['end'].max()).strftime("%d/%m")
-# 5
 ax.set_xticks(xticks)
-ax.set_xticklabels(xticklabels[::7])
+ax.set_xticklabels(xticklabels, rotation=90)
 
-# 6
+ax.xaxis.tick_top()
+ax.xaxis.set_label_position('top')
+
+deadline = dt.datetime(2024, 5, 2)
+deadline_days = (deadline - df['start'].min()).days
+ax.axvline(x=deadline_days, color='red', linestyle='dotted', lw=2)
+
+ax.text(deadline_days + 1, 0.5, 'REPORT DUE 2 MAY 24', rotation=90, verticalalignment='center_baseline', horizontalalignment='left', color='red')
+
 ax.xaxis.grid(True, alpha=0.5)
 
+plt.tight_layout()
+plt.savefig('gantt-chart_.png')
 plt.show()
+
+
