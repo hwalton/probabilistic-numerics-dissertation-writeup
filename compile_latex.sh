cb3b8645fffdaf859c99d2cc35c6439269ff7e0f
hwalton <harveywalton.hw@gmail.com>
2023-12-02 16:30:31 +0000

    comile_latex updated to remove intermediate files

--- compile_latex.sh
+++ compile_latex.sh
@@ -5,3 +5,13 @@ biber dissertation
 xelatex dissertation.tex
 xelatex dissertation.tex
 
+# Clean up auxiliary files
+rm dissertation.aux
+rm dissertation.bbl
+rm dissertation.bcf
+rm dissertation.blg
+rm dissertation.ilg
+rm dissertation.log
+rm dissertation.nlo
+rm dissertation.nls
+# Add any other generated files you want to delete
